91c1ace8864dcff8751db69d7dafcc07bc0e86fe
Valdas <valdaskontenis1@gmail.com.com>
2023-02-25 13:48:55 +0200

    made grid and hover effect

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 'use strict';
 console.log('script.js');
+
+// Nusitaikymai
+const sketchArea = document.getElementById('container');
+
+function makeGrid(cols, rows) {
+  sketchArea.style.setProperty('--grid-cols', cols);
+  sketchArea.style.setProperty('--grid-rows', rows);
+  for (let i = 0; i < cols * rows; i++) {
+    let square = document.createElement('div');
+    square.classList.add('oneGridSquare');
+    sketchArea.append(square);
+    square.addEventListener('mouseover', function colorSquare() {
+      square.style.background = '#333';
+    });
+  }
+}
+
+makeGrid(16, 16);
